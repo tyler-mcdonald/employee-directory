471e143928275e32149d886aae69459a679e817d
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-07-20 14:47:47 -0500

    Add createElement() method

--- js/Employee.js
+++ js/Employee.js
@@ -8,20 +8,4 @@ class Employee {
     this.state = state;
     this.country = country;
   }
-
-  createElement() {
-    const html = `
-      <div class="card">
-        <div class="card-img-container">
-            <img class="card-img" src="${this.image}" alt="${this.firstName} ${this.lastName}'s profile picture">
-        </div>
-        <div class="card-info-container">
-            <h3 id="${this.firstName} ${this.lastName}" class="card-name cap">${this.firstName} ${this.lastName}</h3>
-            <p class="card-text">${this.email}</p>
-            <p class="card-text cap">${this.city}, ${this.state}, ${this.country}</p>
-        </div>
-      </div>
-    `;
-    return html;
-  }
 }
--- js/Page.js
+++ js/Page.js
@@ -12,4 +12,20 @@ class Page {
 
     searchForm.insertAdjacentHTML("afterbegin", form);
   }
+
+  createElement(employee) {
+    const html = `
+      <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${employee.image}" alt="${employee.firstName} ${employee.lastName}'s profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="${employee.firstName} ${employee.lastName}" class="card-name cap">${employee.firstName} ${employee.lastName}</h3>
+            <p class="card-text">${employee.email}</p>
+            <p class="card-text cap">${employee.city}, ${employee.state}, ${employee.country}</p>
+        </div>
+      </div>
+    `;
+    return html;
+  }
 }
