e2c859857e6e4e4ba28ce855de5ad635ecfbb6e9
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-07-20 14:19:59 -0500

    Create Employee class

---
+++ js/Employee.js
@@ -0,0 +1,11 @@
+class Employee {
+  constructor(image, firstName, lastName, email, city, state, country) {
+    this.image = image;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.city = city;
+    this.state = state;
+    this.country = country;
+  }
+}
