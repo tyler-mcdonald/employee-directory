9b0ca59fdbae404ffb85c15dabf04fa73b99c415
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-07-20 21:51:36 -0500

    Update Employee.js

--- js/Employee.js
+++ js/Employee.js
@@ -3,6 +3,7 @@ class Employee {
     this.image = image;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.fullName = `${this.firstName} ${this.lastName}`;
     this.email = email;
     this.city = city;
     this.state = state;
