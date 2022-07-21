7460873ae194793508138b09452c5531efa1a0f9
Tyler McDonald <100496212+tyler-mcdonald@users.noreply.github.com>
2022-07-20 21:51:28 -0500

    Update scripts.js
    Refactor employee objects; add loadModel() function

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
 /** Get and display 12 random users */
 const page = new Page();
 page.loadSearchForm();
 
-const users = "https://randomuser.me/api/?results=3&gender=male";
+const users = "https://randomuser.me/api/?results=3&gender=male"; // remove male filter
 const gallery = document.querySelector("#gallery");
 const employees = [];
 
-// Make a GET request to fetch API
+getJSON(users).then(displayUserInfo).then(addEventListeners);
+
+// Fetch API
 async function getJSON(url) {
   const response = await fetch(url);
   const data = await response.json();
   return data.results;
 }
 
-getJSON(users).then(displayUserInfo).then(addEventListeners);
-
-function newEmployee(object) {
-  const employee = new Employee(
-    object.picture.large,
-    object.name.first,
-    object.name.last,
-    object.email,
-    object.location.city,
-    object.location.state,
-    object.location.country
-  );
-  employees.push(employee);
-  return employee;
-}
-
 // Map over each item and display on page
 async function displayUserInfo(array) {
-  await array.map((user) => {
+  await array.map((employee) => {
     // Store new employee object
-    const employee = newEmployee(user);
+    employees.push(employee);
 
     // Create html element
     const html = page.createElement(employee);
@@ -42,15 +29,11 @@ async function displayUserInfo(array) {
     gallery.insertAdjacentHTML("beforeend", html);
   });
 
+  console.log(employees);
   return await array;
 }
 
 /** Create a modal window */
-// On click user card
-const modal = new Modal(employees);
-modal.log();
-
-// Populate modal information with selected user
 
 /** EVENT LISTENERS */
 
@@ -67,6 +50,13 @@ function addEventListeners() {
   });
 }
 
-function loadModel(name) {
-  console.log(name);
+function loadModel(selection) {
+  const select = employees.filter(
+    (employee) => `${employee.name.first} ${employee.name.last}` === selection
+  )[0]; // Note index 0
+
+  console.log(select);
+
+  const modal = new Modal(select);
+  modal.log(modal.location.street);
 }
